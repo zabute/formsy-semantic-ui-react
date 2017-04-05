3c6c8a0190f09263187b47ef6d2c89a7cf108d9f
zabute <arde987@gmail.com>
2017-04-05 16:34:51 +0300

    Added Tests for Form

---
+++ test/Form.spec.js
@@ -0,0 +1,151 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { assert } from 'chai';
+import { spy } from 'sinon';
+import Form from '../src/Form';
+import { Form as SemanticUIForm } from 'semantic-ui-react';
+import FormsyInput from '../src/FormsyInput';
+
+describe('<Form/>', () => {
+  it('Renders Semantic-UI-React\'s Form', () => {
+    const wrapper = mount(<Form> <Form.Input name="input"/> </Form>);
+    assert.isTrue(wrapper.childAt(0).is(SemanticUIForm));
+  });
+
+  context('When using shorthands', () => {
+    const mountForm = (formElement) => {
+      const TestForm = () => <Form> { formElement } </Form>;
+      return mount(<TestForm/>);
+    };
+
+    it('should render <FormsyInput/> as <Form.Input/>', () => {
+      const wrapper = mountForm(<Form.Input name="formInput"/>);
+      const input = wrapper.find('FormInput');
+      assert.equal(input.length, 1);
+      assert.isTrue(input.is(SemanticUIForm.Input));
+    });
+
+    it('should render <FormsyTextArea/> as <Form.TextArea/>', () => {
+      const wrapper = mountForm(<Form.TextArea name="formInput"/>);
+      const textArea = wrapper.find('FormTextArea');
+      assert.equal(textArea.length, 1);
+      assert.isTrue(textArea.is(SemanticUIForm.TextArea));
+    });
+
+    it('should render <FormsyCheckbox/> as <Form.Checkbox/>', () => {
+      const wrapper = mountForm(<Form.Checkbox name="formInput"/>);
+      const checkbox = wrapper.find('FormCheckbox');
+      assert.equal(checkbox.length, 1);
+      assert.isTrue(checkbox.is(SemanticUIForm.Checkbox));
+    });
+
+    it('should render <FormsyCheckbox/> as <Form.Radio/>', () => {
+      const wrapper = mountForm(<Form.Radio name="formInput"/>);
+      const radio = wrapper.find('FormRadio');
+      assert.equal(radio.length, 1);
+      assert.isTrue(radio.is(SemanticUIForm.Radio));
+    });
+
+    it('should render <FormsyDropdown/> as <Form.Dropdown/>', () => {
+      const wrapper = mountForm(<Form.Dropdown name="formInput"/>);
+      const dropdown = wrapper.find('FormDropdown');
+      assert.equal(dropdown.length, 1);
+      assert.isTrue(dropdown.is(SemanticUIForm.Dropdown));
+    });
+
+    it('should render <FormsySelect/> as <Form.Select/>', () => {
+      const wrapper = mountForm(<Form.Select name="formInput"/>);
+      const select = wrapper.find('FormSelect');
+      assert.equal(select.length, 1);
+      assert.isTrue(select.is(SemanticUIForm.Select));
+    });
+  });
+
+  context('When Submitting', () => {
+    let wrapper;
+    let input;
+    let isValid = false;
+    const onValidSubmit = spy();
+    const onInvalidSubmit = spy();
+    const onSubmit = spy();
+    const validValue = 'john.doe@test.com';
+    const invalidValue = 'Invalid Input';
+    const validationError = 'This is not a valid email';
+    const errorLabel = <div className="error-label"/>;
+
+    const TestForm = () => { // eslint-disable-line
+      return (
+        <Form
+          onValidSubmit={ onValidSubmit }
+          onInvalidSubmit={ onInvalidSubmit }
+          onSubmit={ onSubmit }
+          onValid={ () => isValid = true }
+          onInvalid={ () => isValid = false }
+        >
+          <FormsyInput
+            name="testInput"
+            validations="isEmail"
+            errorLabel={ errorLabel }
+            validationErrors = {{
+              isEmail: validationError,
+            }}
+          />
+        </Form>
+      );
+    };
+
+    const submitForm = () => {
+      wrapper.find('Formsy').node.submit();
+    };
+
+    const validateSubmitCall = (formData) => {
+      assert.ok(onSubmit.calledWith(formData));
+      assert.isFunction(onSubmit.args[0][1]);
+      assert.isFunction(onSubmit.args[0][2]);
+    };
+
+    beforeEach(() => {
+      wrapper = mount(<TestForm/>);
+      input = wrapper.find('FormsyInput');
+    });
+
+    context('When form is Valid', () => {
+      it('should call onSubmit and onValidSubmit when submitted', () => {
+        input.props().setValue(validValue);
+        assert.isTrue(isValid);
+        submitForm();
+        assert.equal(onSubmit.callCount, 1);
+        assert.equal(onValidSubmit.callCount, 1);
+        validateSubmitCall({ testInput: validValue });
+      });
+
+      it('should not call onInvalidSubmit when submitted', () => {
+        input.props().setValue(validValue);
+        assert.isTrue(isValid);
+        submitForm();
+        assert.equal(onSubmit.callCount, 2);
+        assert.equal(onInvalidSubmit.callCount, 0);
+        validateSubmitCall({ testInput: validValue });
+      });
+    });
+
+    context('When form is Invalid', () => {
+      beforeEach(() => input.props().setValue(invalidValue));
+      it('should call onSubmit and onInvalidSubmit when submitted', () => {
+        assert.isFalse(isValid);
+        submitForm();
+        assert.equal(onSubmit.callCount, 3);
+        assert.equal(onValidSubmit.callCount, 2);
+        validateSubmitCall({ testInput: invalidValue });
+      });
+
+      it('should call onValidSubmit when submitted', () => {
+        assert.isFalse(isValid);
+        submitForm();
+        assert.equal(onSubmit.callCount, 4);
+        assert.equal(onValidSubmit.callCount, 2);
+        validateSubmitCall({ testInput: invalidValue });
+      });
+    });
+  });
+});
