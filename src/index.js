6b289c0141b1f3c711f759bff03fbbe2e1067bcd
zabute <arde987@gmail.com>
2017-04-05 16:33:49 +0300

    Add Form to index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-export { default as Input } from './FormsyInput';
-export { default as TextArea } from './FormsyTextArea';
-export { default as Checkbox } from './FormsyCheckbox';
-export { default as Radio } from './FormsyRadio';
-export { default as RadioGroup } from './FormsyRadioGroup';
-export { default as Select } from './FormsySelect';
-export { default as Dropdown } from './FormsyDropdown';
+export Form from './Form';
+export Input from './FormsyInput';
+export TextArea from './FormsyTextArea';
+export Checkbox from './FormsyCheckbox';
+export Radio from './FormsyRadio';
+export RadioGroup from './FormsyRadioGroup';
+export Select from './FormsySelect';
+export Dropdown from './FormsyDropdown';
