5ca3e78623237c7bb8e7db8faaee98f14ce14b5b
zabute <arde987@gmail.com>
2017-04-05 16:32:48 +0300

    Adhere to SUIR's markup

--- src/FormsyCheckbox.js
+++ src/FormsyCheckbox.js
@@ -1,37 +1,34 @@
-import React, { Component, PropTypes, cloneElement } from 'react';
+import React, { Component, PropTypes, createElement, cloneElement } from 'react';
 import { Decorator as formsy } from 'formsy-react';
-import { Checkbox, Radio } from 'semantic-ui-react';
+import { Form, Checkbox, Radio } from 'semantic-ui-react';
+import { filterSuirElementProps } from './utils';
 
 @formsy()
 export default class FormsyCheckbox extends Component {
   static propTypes = {
     name: PropTypes.string.isRequired,
-    as: PropTypes.oneOf(['checkbox', 'radio']),
+    as: PropTypes.oneOf([Form.Checkbox, Form.Radio, Checkbox, Radio]),
     defaultChecked: PropTypes.bool,
     setValue: PropTypes.func.isRequired,
     isValid: PropTypes.func.isRequired,
     getValue: PropTypes.func.isRequired,
     isPristine: PropTypes.func.isRequired,
     required: PropTypes.bool,
-    rootClassName: PropTypes.string,
-    rootStyle: PropTypes.object,
-    className: PropTypes.string,
-    style: PropTypes.object,
     getErrorMessage: PropTypes.func.isRequired,
     errorLabel: PropTypes.element,
   }
 
   static defaultProps = {
-    as: 'checkbox',
+    as: Checkbox,
   }
 
   componentDidMount() {
-    if (this.props.defaultChecked) this.props.setValue(true);
+    const { defaultChecked, setValue } = this.props;
+    if (defaultChecked) setValue(true);
   }
 
-  handleChange(e, input) {
-    this.props.setValue(input.checked);
-    this.setState({ allowErrors: true });
+  handleChange(e, { checked }) {
+    this.props.setValue(checked);
   }
 
   render() {
@@ -41,45 +38,26 @@ export default class FormsyCheckbox extends Component {
       isPristine,
       errorLabel,
       getErrorMessage,
-      rootClassName,
-      rootStyle,
-      className,
-      style,
       getValue,
-      defaultChecked, // eslint-disable-line
-      setValidations, // eslint-disable-line
-      setValue, // eslint-disable-line
-      resetValue, // eslint-disable-line
-      hasValue, // eslint-disable-line
-      getErrorMessages, // eslint-disable-line
-      isFormDisabled, // eslint-disable-line
-      isFormSubmitted, // eslint-disable-line
-      isRequired, // eslint-disable-line
-      showRequired, // eslint-disable-line
-      showError, // eslint-disable-line
-      isValidValue, // eslint-disable-line
-      validations, // eslint-disable-line
-      validationError, // eslint-disable-line
-      validationErrors, // eslint-disable-line
-      ...otherProps,
     } = this.props;
 
-    const props = {
+    const error = !isPristine() && !isValid();
+
+    const checkboxProps = {
       checked: getValue(),
       onChange: ::this.handleChange,
-      className: className,
-      style: style,
-      ...otherProps,
+      ...filterSuirElementProps(this.props),
     };
 
+    if (as === Form.Checkbox || as === Form.Radio) {
+      checkboxProps.error = error;
+    }
+
     return (
-      <div className={ rootClassName } style={ rootStyle }>
-        { cloneElement(as === 'checkbox' ? <Checkbox/> : <Radio/>, { ...props }) }
-        {
-          !isValid() && !isPristine() && errorLabel &&
-            cloneElement(errorLabel, { children: getErrorMessage() })
-        }
-      </div>
+      <Form.Field>
+        { createElement(as, { ...checkboxProps }) }
+        { error && errorLabel && cloneElement(errorLabel, {}, getErrorMessage()) }
+      </Form.Field>
     );
   }
 }
