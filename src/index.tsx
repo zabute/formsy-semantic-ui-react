7c0487567a51c549ce6300e21449890fda329cc1
Felix Mosheev <9304194+felixmosh@users.noreply.github.com>
2026-04-28 18:00:36 +0300

    fix: export types

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 export { default as Form } from './Form';
-export { default as Input } from './FormsyInput';
+export { default as Input, type IFormsyInputProps } from './FormsyInput';
 export { default as TextArea } from './FormsyTextArea';
-export { default as Checkbox } from './FormsyCheckbox';
+export { default as Checkbox, type IFormsyCheckboxProps } from './FormsyCheckbox';
 export { default as Radio } from './FormsyRadio';
-export { default as RadioGroup } from './FormsyRadioGroup';
+export { default as RadioGroup, type IFormsyRadioGroupProps } from './FormsyRadioGroup';
 export { default as Select } from './FormsySelect';
-export { default as Dropdown } from './FormsyDropdown';
+export { default as Dropdown, type IFormsyDropdownProps } from './FormsyDropdown';
